1371dabd0c6ff4b0616709e376d4987f489e39a9
Benjamin Désigné <designebenjamin@gmail.com>
2019-12-11 17:24:04 +0100

    create few User, Prestataire on the UserFixtures for futur test

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -25,15 +25,66 @@ class UserFixtures extends Fixture
             'the_new_password'
         ));
 
-        $subscriberauthor = new User();
-        $subscriberauthor->setEmail("subscriberauthor@monsite.com");
-        $subscriberauthor->setRoles(['ROLE_SUBSCRIBERAUTHOR']);
-        $subscriberauthor->setPassword($this->passwordEncoder->encodePassword(
-            $subscriberauthor,
-            'subscriberpassword'
+
+        $user1 = new User();
+        $user1->setEmail("user1@monsite.com");
+        $user1->setRoles(['ROLE_USER']);
+        $user1->setPassword($this->passwordEncoder->encodePassword(
+            $user1,
+            'userpassword'
         ));
 
-        $manager->persist($subscriberauthor);
+
+        $manager->persist($user1);
+
+
+        $user2 = new User();
+        $user2->setEmail("user2@monsite.com");
+        $user2->setRoles(['ROLE_USER']);
+        $user2->setPassword($this->passwordEncoder->encodePassword(
+            $user2,
+            'userpassword'
+        ));
+
+
+        $manager->persist($user2);
+
+
+        $user3 = new User();
+        $user3->setEmail("user3@monsite.com");
+        $user3->setRoles(['ROLE_USER']);
+        $user3->setPassword($this->passwordEncoder->encodePassword(
+            $user3,
+            'userpassword'
+        ));
+
+
+        $manager->persist($user3);
+
+
+        $prestataire = new User();
+        $prestataire->setEmail("prestataire@monsite.com");
+        $prestataire->setRoles(['ROLE_PRESTATAIRE']);
+        $prestataire->setPassword($this->passwordEncoder->encodePassword(
+            $prestataire,
+            'prestatairepassword'
+        ));
+
+
+        $manager->persist($prestataire);
+
+
+
+        $prestataire1 = new User();
+        $prestataire1->setEmail("subscriberauthor2@monsite.com");
+        $prestataire1->setRoles(['ROLE_PRESTATAIRE']);
+        $prestataire1->setPassword($this->passwordEncoder->encodePassword(
+            $prestataire1,
+            'prestatairepassword'
+        ));
+
+
+        $manager->persist($prestataire1);
 
 
         $admin = new User();
