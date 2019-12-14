8a061d190869c58beddc2e0f6fd8dc83d5933eaa
Axel Redois <axelredois25@gmail.com>
2019-12-14 21:30:11 +0100

    validation assert forms category article and professional

--- src/Entity/Professional.php
+++ src/Entity/Professional.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProfessionalRepository")
@@ -20,16 +21,20 @@ class Professional
 
     /**
      * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Email
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $place;
 
@@ -40,6 +45,7 @@ class Professional
 
     /**
      * @ORM\Column(type="string", length=10)
+     * @Assert\NotBlank
      */
     private $phone;
 
@@ -55,7 +61,6 @@ class Professional
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\BusinessHour", mappedBy="professional", cascade={"persist", "remove"})
-     *
      */
     private $businessHour;
 
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -7,14 +7,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class, ["required" => true, 'constraints' => [new NotBlank()]])
+            ->add('name', TextType::class)
         ;
     }
 
