6ebed114666980bbdcb28c9eda9b49c004ed5387
Axel Redois <axelredois25@gmail.com>
2019-12-20 13:34:47 +0100

    changes for PR

--- src/Form/ProfessionalType.php
+++ src/Form/ProfessionalType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ProfessionalType extends AbstractType
 {
