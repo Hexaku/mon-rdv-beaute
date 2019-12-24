b4c8074f7d696518e87267c2d0bc88bcef45b332
Axel Redois <axelredois25@gmail.com>
2019-12-24 22:44:52 +0100

    fix permissions mobile menu

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -21,6 +21,7 @@ class RegistrationFormType extends AbstractType
             ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                "label" => "J'accepte les conditions générales d'utilisation",
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -31,6 +32,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                "label" => "Mot de passe",
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
