73b7c274c456f6b17de6832028003b4c715971d3
Barré <barre.alexandre44@outlook.fr>
2020-01-06 17:56:10 +0100

    Home create

---
+++ src/Form/HomeType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Home;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HomeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('image')
+            ->add('fileName')
+            ->add('imageFile', Filetype::class, [
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Home::class,
+        ]);
+    }
+}
