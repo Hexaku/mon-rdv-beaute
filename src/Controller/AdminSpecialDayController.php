8ee32e18e5e89b3e8f0c7fa21d2f71bf8366dcc9
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2020-01-08 17:39:48 +0100

    add part links to reach special day page & modal

--- src/Controller/AdminSpecialDayController.php
+++ src/Controller/AdminSpecialDayController.php
@@ -11,17 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * Class AdminSpecialDay
  * @package App\Controller
- * @Route("/admin")
+ *
  */
+//@Route("/admin")
 class AdminSpecialDayController extends AbstractController
 {
     /**
-     * @Route("/special/day", name="admin_special_day")
+     * @Route("admin/special/day", name="admin_special_day")
      */
     public function index(ContactDayRepository $contactDayRepository): Response
     {
+        //this method loads list of people interested by special day (journées bien-être)
         return $this->render("admin/special_day.html.twig", [
             'contactDay' => $contactDayRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("special/day", name="special_day_index")
+     */
+    public function indexShow(): Response
+    {
+        return $this->render("special_day/index.html.twig");
     }
 }
