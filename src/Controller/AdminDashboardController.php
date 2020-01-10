24173c364cc5b9dd349ccada1155988aae3c07e7
Axel Redois <axelredois25@gmail.com>
2020-01-11 00:08:34 +0100

    dashboard objectives is done

---
+++ src/Controller/AdminDashboardController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Dashboard;
+use App\Entity\Professional;
+use App\Entity\Service;
+use App\Form\DashboardType;
+use App\Repository\DashboardRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminDashboardController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_index", methods={"GET","POST"})
+     */
+    public function index(Request $request, DashboardRepository $dashboardRepository): Response
+    {
+        $repositoryService = $this->getDoctrine()->getRepository(Service::class);
+        $services = $repositoryService->findAllServices();
+
+        $repoProfessional = $this->getDoctrine()->getRepository(Professional::class);
+        $professionals = $repoProfessional->findAllProfessionals();
+
+        $dashboard = new Dashboard();
+        $form = $this->createForm(DashboardType::class, $dashboard);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($dashboard);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_index');
+        }
+
+        return $this->render("admin/index.html.twig", [
+            "services" => $services,
+            "professionals" => $professionals,
+            'dashboard' => $dashboard,
+            'dashboards' => $dashboardRepository->findAll(),
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_dashboard_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Dashboard $dashboard): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$dashboard->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($dashboard);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_index');
+    }
+}
---
+++ src/Twig/ObjectiveExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class ObjectiveExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('objective', [$this, 'formatObjective']),
+        ];
+    }
+
+    public function formatObjective($objective)
+    {
+        switch ($objective) {
+            case 1:
+                return "Prestations";
+                break;
+            case 2:
+                return "Professionnels";
+                break;
+            case 3:
+                return "Membres";
+                break;
+            case 4:
+                return "Clients";
+                break;
+        }
+    }
+}
