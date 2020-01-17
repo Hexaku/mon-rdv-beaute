abd4761973ae6f2c56821f914095e532968557e5
Axel Redois <axelredois25@gmail.com>
2020-01-17 14:03:39 +0100

    securising forms

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -6,7 +6,6 @@ use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
--- src/Service/FormatYoutubeLink.php
+++ src/Service/FormatYoutubeLink.php
@@ -5,10 +5,8 @@ namespace App\Service;
 class FormatYoutubeLink
 {
     /*
-    convert videos link like :
-    https://www.youtube.com/watch?v=OztPdc3pwYU
-    TO
-    https://www.youtube.com/embed/OztPdc3pwYU
+    convert videos link like : https://www.youtube.com/watch?v=OztPdc3pwYU
+    TO => https://www.youtube.com/embed/OztPdc3pwYU
     */
     public function format(string $link): ?string
     {
