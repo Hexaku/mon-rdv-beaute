7290eec2e43e40891b07f0973e6f0c07c763f2c3
Axel Redois <axelredois25@gmail.com>
2020-01-18 10:15:46 +0100

    change colors in full calendar js, make past today date unclickable

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -34,20 +34,34 @@ document.addEventListener('DOMContentLoaded', () => {
             },
         ],
         dateClick: (info) => {
-            fetch(`/service/${calendarEl.getAttribute('service')}/${info.dateStr}`)
-                .then(response => response.json())
-                .then((result) => {
-                    if (result.length === 0) {
-                        $('.modal-body').html('Il n\'y a pas d\'horaires disponible pour ce jour');
-                    } else {
-                        let hours = '';
-                        result.forEach((item, index) => {
-                            hours += `<a href="/booking/${calendarEl.getAttribute('service')}/${info.dateStr}/${result[index]}" class="btn btn-pink">${result[index]}</a>`;
-                        });
-                        $('.modal-body').html(hours);
-                    }
-                    $('#exampleModalCenter').modal();
-                });
+            /* CREATE NEW DATE TO COMPARE IF DATE CLICKED IS PAST OR NOT */
+            const today = new Date();
+            today.setHours(0);
+            /* IF PAST NO EVENT IS DONE, ELSE A MODAL OPEN WITH AVAILABLE HOURS FOR THIS SERVICE */
+            if (today < info.date) {
+                fetch(`/service/${calendarEl.getAttribute('service')}/${info.dateStr}`)
+                    .then(response => response.json())
+                    .then((result) => {
+                        if (result.length === 0) {
+                            $('.modal-body').html('Il n\'y a pas d\'horaires disponible pour ce jour');
+                        } else {
+                            let hours = '';
+                            result.forEach((item, index) => {
+                                hours += `<a href="/booking/${calendarEl.getAttribute('service')}/${info.dateStr}/${result[index]}" class="btn btn-pink">${result[index]}</a>`;
+                            });
+                            $('.modal-body').html(hours);
+                        }
+                        $('#exampleModalCenter').modal();
+                    });
+            }
+        },
+        dayRender: (dayRenderInfo) => {
+            const today = new Date();
+            today.setHours(0);
+            if (today > dayRenderInfo.date) {
+                // eslint-disable-next-line no-param-reassign
+                dayRenderInfo.el.bgColor = '#f4f4f4';
+            }
         },
         header: {
             left: 'prev,next today',
