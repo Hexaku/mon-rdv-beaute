51355975f7143103d87dd049d587ee60345b994b
Barré <barre.alexandre44@outlook.fr>
2020-01-20 15:08:22 +0100

    mailerAdmin ok

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Service;
+use App\Form\ServiceType;
 use App\Repository\BookingRepository;
 use App\Repository\BusinessHourRepository;
+use App\Service\MailerSender;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use DateInterval;
